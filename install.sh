2c8c949050658809b2e7d8d0a7deb466b408400f
K3A <spam@k3a.me>
2016-07-27 17:31:46 +0200

    install update, gitconfig

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+# directory in which this script resides
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-for f in $(ls -A --ignore ".git*" --ignore "install.sh" "$DIR"); do
+# update submodules
+git submodule update --init --recursive
+
+# for each file in the directory
+for f in $(ls -A --ignore ".git" --ignore ".gitmodules" --ignore "install.sh" "$DIR"); do
 	echo "$f -> $HOME/$f"
 	cp -R "$DIR/$f" "$HOME/"
 done
