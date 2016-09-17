2693625894df14197aa4099f369a06800e973838
Mario <spam@k3a.me>
2016-09-17 20:14:39 +0200

    ..

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 git submodule update --init --recursive
 
 # for each file in the directory
-for f in $(ls -A --ignore ".git" --ignore ".gitmodules" --ignore "install.sh" "$DIR"); do
+for f in $(ls -A "$DIR/files"); do
 	echo "$f -> $HOME/$f"
-	cp -R "$DIR/$f" "$HOME/"
+	cp -R "$DIR/files/$f" "$HOME/"
 done
 
