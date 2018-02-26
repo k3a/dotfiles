bf4ec3aeebdb38de3d620e92cd236b6ad7631684
Mario Hros <spam@k3a.me>
2018-02-27 00:39:59 +0100

    droid mtp

--- files/.config/fish/config.fish
+++ files/.config/fish/config.fish
@@ -3,3 +3,5 @@ set PATH $PATH ~/bin ~/go/bin ~/.cargo/bin/ ~/.config/yarn/global/node_modules/.
 function open
 	nohup xdg-open $argv >/dev/null 2>&1 &
 end
+
+alias droid.mtp="adb shell setprop sys.usb.config mtp,adb"
